21bd991357ed5a5854281ea872038b7343c942ba
yash52003 <117395796+yash52003@users.noreply.github.com>
2024-05-16 16:55:14 +0530

    The final commit after adding the middlewares and cookie parser

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,8 @@ const {auth , isStudent , isAdmin } = require("../middlewares/auth");
 router.post("/login" , login);
 router.post("/signup" , signup);
 
+const User = require("../Model/User");
+
 // router.post function is been used for mapping the function {handler} to  the new path
 
 //Now we will start writing our protected routes
@@ -35,6 +37,39 @@ router.get("/admin" , auth , isAdmin , (req , res) => {
         message : "Welcome to the protected route for the admin",
     })
 })
+
+router.get("/getEmail" , auth , async (req , res) => {
+
+    try{
+        const id = req.user.id;
+        console.log("ID" , id);
+        const user = await User.findById({id});
+
+        res.status(200).json({
+            success : true,
+            user : user,
+            message : "Welcome to the email route",
+        })
+    }catch(error){
+
+        res.status(500).json({
+            success:false,
+            error: error.message,
+            message : "The code has been destroyed",
+        })
+
+
+    }
+
+
+
+    console.log("ID: ", id);
+    res.json({
+        success : true,
+        id : id,
+        message : "Welcome to the protected route for the admin",
+    });
+});
 
 //How do we get to know the logined user is the student when we passed the token  to the login user in that token there was onething known as payload and in the payload we have passed the role module so we will take the token decrypt it get the role from it and then Authorise the student 
 
